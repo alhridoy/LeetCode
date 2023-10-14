8bdb67eebd35ab3592942809d9d39856772132a3
Al-Ekram Elahee Hridoy <aliqramalaheehridoy@gmail.com>
2023-10-14 00:41:22 -0600

    Time: 802 ms (33.74%), Space: 22.3 MB (62.35%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -15,6 +15,15 @@ class Solution:
                     count.pop(f)
             res = max(res, total)
         return res
+    
+    '''
+    - Initialize a hash map to keep track of fruit counts and a variable for the total number of fruits.
+- Use a sliding window approach with two pointers (left and right) to traverse the array.
+- Increment the count of the current fruit and total when encountering a fruit.
+- Ensure the window remains valid (at most two distinct fruit types) by popping from the left if needed.
+- Update the result with the maximum total fruit count encountered during traversal.
+- Return the result as the length of the longest valid subarray.
+    '''
                     
             
         
