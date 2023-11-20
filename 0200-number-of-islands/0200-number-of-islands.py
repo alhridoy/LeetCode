65e803d91e768fa1e618fea5f5aa2c6b3abf0ff2
Al-Ekram Elahee Hridoy <aliqramalaheehridoy@gmail.com>
2023-11-19 21:22:21 -0700

    Time: 252 ms (97.72%), Space: 19 MB (55.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -26,4 +26,47 @@ class Solution:
                     #sink the whole island
                     sinkIsland(row,col)
         return num_of_islands
-        
+        '''
+        Time Complexity 
+�
+(
+�
+�
+)
+O(nm). This might be surprising because there are 2 for loops, and then an additional recursion on sinkIsland. But notice that the total number of times sinkIsland gets called is always 
+≤
+�
+(
+�
+�
+)
+≤O(nm) since once it visits a location, it never visits it again. This means we do 
+�
+(
+�
+�
+)
+O(nm) operations for the for loop, and 
+�
+(
+�
+�
+)
+O(nm) for sinkIsland, which is a total of 
+�
+(
+�
+�
+)
+O(nm).
+
+Space Complexity 
+�
+(
+1
+)
+O(1). Technically we're storing 
+�
+�
+nm entries to mark each island as visited, but none of that is extra space. It's all being done using the space already given to us in the grid.
+        '''
